4bc7b84947b6383ce54330e1a1aa468a50b4f980
inzeshtahir <inzesh14@gmail.com>
2024-08-20 19:34:02 -0400

    Add files via upload

---
+++ DataService.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+
+namespace EventHub;
+
+public class DataService
+{
+    private static string connectionString = "Server=localhost;Database=EventHub;Uid=root;Pwd=";
+    public static async Task<List<T>> LoadData<T, U>(string sql, U parameters)
+    {
+        using (var conn = new MySqlConnection(connectionString))
+        {
+            var rows = await conn.QueryAsync<T>(sql, parameters);
+            return rows.ToList();
+        }
+    }
+
+    public static async Task<int> SaveData<U>(string sql, U parameters)
+    {
+        using (var conn = new MySqlConnection(connectionString))
+        {
+            return await conn.ExecuteAsync(sql, parameters);
+        }
+    }
+}
---
+++ Event.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventHub;
+
+public class Event
+{
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(100)]
+    public string Title { get; set; } = "";
+    [Required]
+    public DateTime Date { get; set; } = DateTime.Now;
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = "";
+}
